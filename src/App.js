c485080360e5db16aabbea0d22304b6b6c9ea000
khajornkiat <khajornkiat.us@ols.co.th>
2021-10-31 19:38:25 +0700

    [edit] build with buildspace

--- src/App.js
+++ src/App.js
@@ -1,25 +1,212 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import twitterLogo from './assets/twitter-logo.svg';
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
-function App() {
+import myEpicNft from './utils/MyEpicNFT.json';
+
+// Constants
+const TWITTER_HANDLE = 'niceusap';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const OPENSEA_LINK = 'https://testnets.opensea.io/collection/squarenft-cidctpylfh';
+const TOTAL_MINT_COUNT = 50;
+
+const CONTRACT_ADDRESS = "0xa7A1a296Dcd8d140929052cc3f2c6dB01281e921"
+
+const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [isMining, setIsMining] = useState(false)
+  const [mintedNFT, setMintedNFT] = useState({success: false, link: ''})
+  const [currentMinted, setCurrentMinted] = useState(0)
+
+  const checkIfWalletIsConnected = async () => {
+    //First make sure we have access to windows.ethereum
+    const { ethereum } = window;
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    // Check if we're authorized to access the user's wallet
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    // User can have multiple authorized accounts, we grab the first one if its there!
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account)
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and ALREADY had their wallet connected + authorized.
+      setupEventListener()
+    } else {
+      console.log("No authorized account found")
+    }
+  }
+
+  // Implement your connectWallet method here
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      let chainId = await ethereum.request({ method: 'eth_chainId' });
+      console.log("Connected to chain " + chainId);
+
+      // String, hex code of the chainId of the Rinkebey test network
+      const rinkebyChainId = "0x4"; 
+      if (chainId !== rinkebyChainId) {
+        alert("You are not connected to the Rinkeby Test Network!");
+      }
+
+      // Fancy method to request access to account.
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      // Boom! This should print out public address once we authorize Metamask.
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0])
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+      setMintedNFT({success: false, link: ''})
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+        let currentNFTsNumber = await connectedContract.getTotalNFTsMintedSoFar();
+        setCurrentMinted(currentNFTsNumber.toNumber())
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          setMintedNFT({
+            success: true,
+            link: `https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          })
+          setCurrentMinted(currentNFTsNumber.toNumber() + 1)
+          // alert(`Hey there! We've minted your NFT. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: <https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}>`)
+        });
+
+        console.log("Setup event listener!")
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const askContractToMintNft = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+        let currentNFTsNumber = await connectedContract.getTotalNFTsMintedSoFar();
+        setCurrentMinted(currentNFTsNumber.toNumber())
+        console.log("Going to pop wallet now to pay gas...")
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+        setIsMining(true)
+        console.log("Mining... please wait.")
+        await nftTxn.wait();
+        setIsMining(false)
+        console.log(nftTxn);
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+
+      } else {
+        setIsMining(false)
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // This runs our function when the page loads.
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+  // Render Methods
+  const renderNotConnectedContainer = () => (
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
+      Connect to Wallet
+    </button>
+  );
+
+  const renderMintUI = () => (
+    <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+      {isMining ? <span><i className="fa fa-circle-o-notch fa-spin"></i>Mining</span> : 'Mint NFT'}
+    </button>
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">NFT terminology Collection</p>
+          <p className="sub-text">
+            Each unique. Each beautiful. Discover your NFT today.
+          </p>
+          <p className="sub-text gradient-sub-text">{TOTAL_MINT_COUNT !== currentMinted ? `${currentMinted}/${TOTAL_MINT_COUNT} NFTs minted so far` : "Sold out"}</p>
+          {currentAccount === "" ? renderNotConnectedContainer() : TOTAL_MINT_COUNT !== currentMinted ? renderMintUI() : ""}
+          <p className="sub-text">
+            {mintedNFT.success ? <span>🎉 Congratulations! We've minted your NFT.<br/>
+            <a 
+              href={mintedNFT.link}
+              target="_blank"
+              rel="noreferrer">See it on Opensea</a></span> : ''}
+          </p>
+          
+        </div>
+        <div className="footer-container">
+          <div className="footer-link-item">
+            <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+            <a
+              className="footer-text"
+              href={TWITTER_LINK}
+              target="_blank"
+              rel="noreferrer"
+            >{`@${TWITTER_HANDLE}`}</a>
+          </div>
+          <div className="footer-link-item">
+            <img alt="Opensea Logo" className="twitter-logo" src="https://storage.googleapis.com/opensea-static/Logomark/Logomark-Blue.svg" />
+            <a
+              className="footer-text"
+              href={OPENSEA_LINK}
+              target="_blank"
+              rel="noreferrer"
+            >{`View Collection`}</a>
+          </div>
+          
+        </div>
+        
+      </div>
     </div>
   );
-}
+};
 
 export default App;
